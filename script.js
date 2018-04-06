30106d71dccd331afceef781ef0b208dbe1b9b73
Darthseid <jared.mcvey@yahoo.com>
2018-04-06 14:40:52 -0500

    Extracted javascript.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("Hello, World!");
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+     <title>My first Javascript Lab</title>
+      <link rel="stylesheet" href="style.css"> 
+      <script src="script.js" async=""></script>
+      <script language="javascript" src="script.js" type="text/javascript"></script>
+  </head>
+  <body>
+       <h1 class="centered">ITMD-361 - Lab 6 - Basic JavaScript Event Handling and DOM</h1>
+      <h3 class="centered">jmcvey1@hawk.iit.edu</h3>
+       <h2 class="centered">Jared McVey</h2> 
+    <div id="contain">
+ <input id="entry" type="text" name="textentry" placeholder="Enter Here">
+      <div id="button">Alert Me</div>
+    <h2 id="output" class="centered">What you typed</h2>
+  </div>
+  </body>
+</html>
+
