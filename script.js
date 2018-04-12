c221c6777fd5d323cfc8d16ae68ab04ee59172e4
Henry Post <HenryFBP@gmail.com>
2018-04-11 20:53:42 -0500

    properly use ondomload

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function myFunction() {
-   document.getElementById("entrybutton").addEventListener("click", function() {
-			var txt = document.getElementById("entry").value;
-			document.getElementById("output").innerHTML = txt;
-			alert("Jared McVey: " + txt)})
-}
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+  document.getElementById("entrybutton").addEventListener("click", function() {
+  
+    var txt = document.getElementById("entry").value;
+
+    document.getElementById("output").innerHTML = txt;
+
+    console.log("Jared McVey: " + txt);
+
+  });
+  
+});
